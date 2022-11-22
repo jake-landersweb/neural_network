6ab7e24f80c57ac54ffbc5bedcc3c0b4be53d41e
Jake Landers <jake@landersweb.com>
2022-11-22 01:07:44 -0800

    removed build folder

--- flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods-Runner-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/jakelanders/code/neural_network/flutter/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m \
-  /Users/jakelanders/code/neural_network/flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap
